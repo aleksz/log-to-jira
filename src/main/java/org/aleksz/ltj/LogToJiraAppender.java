b02dcb65fe675ae0e2a4a049d84276ae23a2fd02
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2010-09-20 16:54:17 +0300

    setting up initial project structure as maven project

---
+++ src/main/java/org/aleksz/ltj/LogToJiraAppender.java
@@ -0,0 +1,71 @@
+package org.aleksz.ltj;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.ErrorHandler;
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class LogToJiraAppender implements Appender {
+
+	public void addFilter(Filter arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void clearFilters() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doAppend(LoggingEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ErrorHandler getErrorHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Filter getFilter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Layout getLayout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean requiresLayout() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setErrorHandler(ErrorHandler arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setLayout(Layout arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setName(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
