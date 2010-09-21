09888ce83348234a59e2dd6daf8e5943114903f1
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2010-09-21 23:21:10 +0300

    Switched to localhost testing

--- src/test/java/org/aleksz/ltj/JiraSoapServiceTest.java
+++ src/test/java/org/aleksz/ltj/JiraSoapServiceTest.java
@@ -1,5 +1,6 @@
 package org.aleksz.ltj;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.rmi.RemoteException;
 
@@ -9,13 +10,12 @@ import org.junit.Test;
 
 public class JiraSoapServiceTest {
 
-	String baseUrl = "http://jira.atlassian.com/rpc/soap/jirasoapservice-v2";
-
 	@Test
-	public void test() throws ServiceException, RemoteAuthenticationException, RemoteException, RemoteException {
+	public void test() throws ServiceException, RemoteAuthenticationException, RemoteException, RemoteException, MalformedURLException {
+		URL jira = new URL("http://localhost:2990/jira/rpc/soap/jirasoapservice-v2");
 		JiraSoapServiceServiceLocator jiraSoapServiceServiceLocator = new JiraSoapServiceServiceLocator();
-		JiraSoapService jirasoapservice = jiraSoapServiceServiceLocator.getJirasoapserviceV2();
-		String login = jirasoapservice.login("aleksandr.zuikov", "");
+		JiraSoapService jirasoapservice = jiraSoapServiceServiceLocator.getJirasoapserviceV2(jira);
+		String login = jirasoapservice.login("admin", "admin");
 		jirasoapservice.logout(login);
 	}
 }
