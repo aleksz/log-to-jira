ab5182f25fd5e72a4c7a9d3289b20d736ed43205
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2010-09-28 17:30:38 +0300

    Removed system properties logging, will come back to it later

--- src/main/java/org/aleksz/ltj/BugReport.java
+++ src/main/java/org/aleksz/ltj/BugReport.java
@@ -1,7 +1,5 @@
 package org.aleksz.ltj;
 
-import java.util.Map.Entry;
-
 import org.apache.log4j.spi.LoggingEvent;
 
 public class BugReport {
@@ -29,15 +27,6 @@ public class BugReport {
 
 		for (String line : loggingEvent.getThrowableStrRep()) {
 			result.append(line);
-			result.append("\n");
-		}
-
-		result.append("\n");
-
-		for (Entry<Object, Object> e : System.getProperties().entrySet()) {
-			result.append(e.getKey());
-			result.append(" = ");
-			result.append(e.getValue());
 			result.append("\n");
 		}
 
