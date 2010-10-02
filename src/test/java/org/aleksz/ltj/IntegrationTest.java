e172af894c4dd99fc0b77d3f992e6565368315b3
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2010-10-02 10:29:45 +0300

    added assertions to integration tests

--- src/test/java/org/aleksz/ltj/IntegrationTest.java
+++ src/test/java/org/aleksz/ltj/IntegrationTest.java
@@ -1,6 +1,20 @@
 package org.aleksz.ltj;
 
+import static junit.framework.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.rpc.ServiceException;
+
+import org.aleksz.ltj.soap.JiraSoapService;
+import org.aleksz.ltj.soap.JiraSoapServiceServiceLocator;
+import org.aleksz.ltj.soap.RemoteAuthenticationException;
+import org.aleksz.ltj.soap.RemoteException;
+import org.aleksz.ltj.soap.RemoteIssue;
 import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -8,14 +22,41 @@ public class IntegrationTest {
 
 	private static final Logger LOG = Logger.getLogger(IntegrationTest.class);
 
-	@Test
-	public void logUniqueException() {
-		LOG.error("Tech error nr " + System.currentTimeMillis());
+	private JiraSoapService jiraSoapService;
+	private String token;
+
+	@Before
+	public void init() throws MalformedURLException, ServiceException,
+			RemoteAuthenticationException, RemoteException, java.rmi.RemoteException {
+
+		JiraSoapServiceServiceLocator locator = new JiraSoapServiceServiceLocator();
+		this.jiraSoapService = locator.getJirasoapserviceV2(
+				new URL("http://localhost:2990/jira/rpc/soap/jirasoapservice-v2"));
+		token = jiraSoapService.login("admin", "admin");
+	}
+
+	@After
+	public void logout() throws java.rmi.RemoteException {
+		jiraSoapService.logout(token);
 	}
 
 	@Test
-	public void logErrorMessage() {
-		LOG.error("This is error message");
+	public void logUniqueSummary() throws RemoteException, java.rmi.RemoteException {
+		String message = "Tech error nr " + System.currentTimeMillis();
+		LOG.error(message);
+		String JQL = "summary ~ \"\\\"" + message + "\\\"\"";
+		RemoteIssue[] result = jiraSoapService.getIssuesFromJqlSearch(token, JQL, 2);
+		assertEquals(1, result.length);
+	}
+
+	@Test
+	public void logDuplicateSummary() throws RemoteException, java.rmi.RemoteException {
+		String message = "This is error message";
+		LOG.error(message);
+		LOG.error(message);
+		String JQL = "summary ~ \"\\\"" + message + "\\\"\"";
+		RemoteIssue[] result = jiraSoapService.getIssuesFromJqlSearch(token, JQL, 2);
+		assertEquals(1, result.length);
 	}
 
 	@Test
